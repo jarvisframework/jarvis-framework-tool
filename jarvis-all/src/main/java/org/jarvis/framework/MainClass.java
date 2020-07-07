2d0d4c886ca0cba8018f8f70745c54e08ce4d477
Karl <773995514@qq.com>
2020-07-07 12:09:43 +0800

    1、maven依赖优化；2、修改common模块；3、添加所有独立模块聚合依赖模块。

---
+++ jarvis-all/src/main/java/org/jarvis/framework/MainClass.java
@@ -0,0 +1,14 @@
+package org.jarvis.framework;
+
+/**
+ * <p>所有模块依赖包</p>
+ *
+ * @author 王涛
+ * @since 1.0, 2020-07-07 12:01:53
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
