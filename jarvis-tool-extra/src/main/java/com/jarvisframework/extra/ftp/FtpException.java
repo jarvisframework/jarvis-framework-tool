0a2a28342d3320f82ae39fc1ccc4191e4bae4f92
Karl <773995514@qq.com>
2020-07-15 09:54:31 +0800

    提交三方模块和FTP工具类。

---
+++ jarvis-tool-extra/src/main/java/com/jarvisframework/extra/ftp/FtpException.java
@@ -0,0 +1,35 @@
+package com.jarvisframework.extra.ftp;
+
+import cn.hutool.core.exceptions.ExceptionUtil;
+import cn.hutool.core.util.StrUtil;
+
+/**
+ * <p>Ftp异常类</p>
+ *
+ * @author 王涛
+ * @since 1.0, 2020-07-14 11:58:21
+ */
+public class FtpException extends RuntimeException {
+
+    private static final long serialVersionUID = 8034331991006644355L;
+
+    public FtpException(Throwable e) {
+        super(ExceptionUtil.getMessage(e), e);
+    }
+
+    public FtpException(String message) {
+        super(message);
+    }
+
+    public FtpException(String messageTemplate, Object... params) {
+        super(StrUtil.format(messageTemplate, params));
+    }
+
+    public FtpException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+
+    public FtpException(Throwable throwable, String messageTemplate, Object... params) {
+        super(StrUtil.format(messageTemplate, params), throwable);
+    }
+}
---
+++ jarvis-tool-extra/src/test/java/com/jarvisframework/extra/ftp/FtpClientTest.java
@@ -0,0 +1,29 @@
+package com.jarvisframework.extra.ftp;
+
+import com.jarvisframework.tool.core.lang.Console;
+import com.jarvisframework.tool.core.util.StringUtils;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * <p>FTP客户端测试类</p>
+ *
+ * @author 王涛
+ * @since 1.0, 2020-07-14 12:08:34
+ */
+public class FtpClientTest {
+
+    @Test
+    public void Test() throws IOException {
+        //FtpClient client = new FtpClient("61.164.128.178",12121,"CARRIER","SEMIRpASS", FtpModeEnum.Passive);
+        FtpClient client = new FtpClient("ftp.unisdigital.com",21,"prd-xinyitai","xyt02@PWD");
+        // System.out.println(client.cd("/download/"));
+        // System.out.println(client.ls("/"));
+        // InputStream stream = client.downloadStream("/","8496-20071410360367550-1.png");
+
+        Console.log(client.listNames());
+        Console.log(client.downloadDataTableByTextFile("/downloadbak/", "333.txt", StringUtils.TAB));
+    }
+
+}
